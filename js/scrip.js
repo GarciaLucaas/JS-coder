ee2b178365874a6208b79bf0f76d2a09639fa05c
Lucasmartinez <garcia.lucaset36@gmail.com>
2022-08-08 22:34:52 -0300

    Evento

--- js/scrip.js
+++ js/scrip.js
@@ -13,26 +13,32 @@
 // }
 // dividir()
 
-function Combo (producto, adicional, bebida){
-    this.producto = producto
-    this.adicional = adicional
-    this.bebida = bebida
-}
-const arrayCombo = [];
-let esc = "no"
-while (esc != "ESC"){
-    let producto = prompt ("ingrese la hamburguesa que desea (Triple, Doble, Simple)")
-    let adicional = prompt ("ingrese el adicional que desea (Papas, Aros, Ensalada)")
-    let bebida = prompt ("ingrese la bebida (Coca-Cola, Sprite, Fanta)")
-    const combo1 = new Combo (producto, adicional, bebida);
-    function pedido(){
-        alert("Orden: Hamburguesa " + producto + " Con " + adicional + " Con " + bebida)
-    }
-    arrayCombo.push(combo1)
+// function Combo (producto, adicional, bebida){
+//     this.producto = producto
+//     this.adicional = adicional
+//     this.bebida = bebida
+// }
+// const arrayCombo = [];
+// let esc = "no"
+// while (esc != "ESC"){
+//     let producto = prompt ("ingrese la hamburguesa que desea (Triple, Doble, Simple)")
+//     let adicional = prompt ("ingrese el adicional que desea (Papas, Aros, Ensalada)")
+//     let bebida = prompt ("ingrese la bebida (Coca-Cola, Sprite, Fanta)")
+//     const combo1 = new Combo (producto, adicional, bebida);
+//     function pedido(){
+//         alert("Orden: Hamburguesa " + producto + " Con " + adicional + " Con " + bebida)
+//     }
+//     arrayCombo.push(combo1)
 
-    console.log(arrayCombo)
+//     console.log(arrayCombo)
 
-    esc = prompt("desea salir escriba ESC")
-    esc.toLowerCase();
-    pedido();
-}
+//     esc = prompt("desea salir escriba ESC")
+//     esc.toLowerCase();
+//     pedido();
+// }
+
+const pedido2 = document.getElementById("combo");
+precio = 1200;
+pedido2.addEventListener("click", () =>{
+    alert("Hizo click en el combo. Precio final" + precio);
+})
