9875addd26e26d4ecdba9c0554ac0f728040018d
Lucasmartinez <garcia.lucaset36@gmail.com>
2022-08-26 18:51:19 -0300

    2 entrega

---
+++ js/desafio.js
@@ -0,0 +1,44 @@
+// function dividir(){
+//     let promedio = edadfinal/i;
+//     alert("Promedio de edades : "+ promedio+" años")
+// } 
+// let edadUsuario = prompt("Para salir escriba ESC")
+// i = 0;
+// edad = 0;
+// while( edadUsuario != "ESC"){
+//     edad2 = parseInt(prompt("Ingrese edad del usuario"))
+//     edadUsuario = prompt("Ingrese ESC O '+' para sumar")
+//     edadfinal = edad + edad2;
+//     i++;
+// }
+// dividir()
+
+/* function Combo (producto, adicional, bebida){
+    this.producto = producto
+    this.adicional = adicional
+    this.bebida = bebida
+}
+const arrayCombo = [];
+let esc = "no"
+while (esc != "ESC"){
+    let producto = prompt ("ingrese la hamburguesa que desea (Triple, Doble, Simple)")
+    let adicional = prompt ("ingrese el adicional que desea (Papas, Aros, Ensalada)")
+    let bebida = prompt ("ingrese la bebida (Coca-Cola, Sprite, Fanta)")
+    const combo1 = new Combo (producto, adicional, bebida);
+    function pedido(){
+        alert("Orden: Hamburguesa " + producto + " Con " + adicional + " Con " + bebida)
+    }
+    arrayCombo.push(combo1)
+
+    console.log(arrayCombo)
+
+    esc = prompt("desea salir escriba ESC")
+    esc.toLowerCase();
+    pedido();
+}
+
+const pedido2 = document.getElementById("combo");
+precio = 1200;
+pedido2.addEventListener("click", () =>{
+    alert("Hizo click en el combo. Precio final" + precio);
+}) */
--- js/scrip.js
+++ js/scrip.js
@@ -1,44 +1,27 @@
-// function dividir(){
-//     let promedio = edadfinal/i;
-//     alert("Promedio de edades : "+ promedio+" años")
-// } 
-// let edadUsuario = prompt("Para salir escriba ESC")
-// i = 0;
-// edad = 0;
-// while( edadUsuario != "ESC"){
-//     edad2 = parseInt(prompt("Ingrese edad del usuario"))
-//     edadUsuario = prompt("Ingrese ESC O '+' para sumar")
-//     edadfinal = edad + edad2;
-//     i++;
-// }
-// dividir()
+const Comprar = document.querySelectorAll(".combo")
+let carrito = []
 
-// function Combo (producto, adicional, bebida){
-//     this.producto = producto
-//     this.adicional = adicional
-//     this.bebida = bebida
-// }
-// const arrayCombo = [];
-// let esc = "no"
-// while (esc != "ESC"){
-//     let producto = prompt ("ingrese la hamburguesa que desea (Triple, Doble, Simple)")
-//     let adicional = prompt ("ingrese el adicional que desea (Papas, Aros, Ensalada)")
-//     let bebida = prompt ("ingrese la bebida (Coca-Cola, Sprite, Fanta)")
-//     const combo1 = new Combo (producto, adicional, bebida);
-//     function pedido(){
-//         alert("Orden: Hamburguesa " + producto + " Con " + adicional + " Con " + bebida)
-//     }
-//     arrayCombo.push(combo1)
 
-//     console.log(arrayCombo)
-
-//     esc = prompt("desea salir escriba ESC")
-//     esc.toLowerCase();
-//     pedido();
-// }
-
-const pedido2 = document.getElementById("combo");
-precio = 1200;
-pedido2.addEventListener("click", () =>{
-    alert("Hizo click en el combo. Precio final" + precio);
+Comprar.forEach(btn =>{
+    btn.addEventListener("click",agregarcarrito)
 })
+
+function agregarcarrito(e){
+    const button = e.target
+    const producto = button.closest(".card")
+    const productoTitulo = producto.querySelector('.card-title').textContent;
+    // const productoPrecio = producto.querySelector('.precio').textContent;
+    const productoImg = producto.querySelector('.card-img-top').src;
+    const newProducto = {
+        titulo: productoTitulo,
+        // precio: itemPrice,
+        img: productoImg,
+        cantidad: 1
+        }
+        carrito.push(newProducto)
+        console.log(addLocalStorage())
+}
+function addLocalStorage(){
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+  console.log(carrito)
+}
