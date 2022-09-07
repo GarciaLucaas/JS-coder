542ebf9f37143a0405e2f4898c1dcdda9b06ef83
Lucasmartinez <garcia.lucaset36@gmail.com>
2022-09-07 00:05:04 -0300

    Proyecto Final

--- js/desafio.js
+++ js/desafio.js
@@ -42,3 +42,83 @@ precio = 1200;
 pedido2.addEventListener("click", () =>{
     alert("Hizo click en el combo. Precio final" + precio);
 }) */
+
+// ------------------------------
+// class Producto{
+//     constructor(nombre, precio,img){
+//         this.nombre = nombre;
+//         this.precio = precio;
+//         this.img = img;
+//     }
+// }
+// const Comprar = document.querySelectorAll(".combo")
+// let carrito = []
+
+
+// Comprar.forEach(btn =>{
+//     btn.addEventListener("click",agregarcarrito)
+// })
+// const productos = JSON.stringifl(carrito)
+
+// const contenedorProd = document.getElementById("contenedorProd")
+// productos.forEach((producto, indice) =>{
+//     contenedorProd.innerHTML +=`
+//     <div class="card" style="width: 18rem;" id="producto${indice}>
+//         <img src="${producto.img}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//             <h5 class="card-title">${producto.nombre}</h5>
+//             <p class="card-text">${producto.precio}</p>
+//             <a href="#" class="btn btn-primary">Comprar</a>
+//         </div>
+//     </div>`
+// })
+
+// function agregarcarrito(e){
+//     const button = e.target
+//     // const producto = button.closest(".card")
+//     // const productoTitulo = producto.querySelector('.card-title').textContent;
+//     // // const productoPrecio = producto.querySelector('.precio').textContent;
+//     // const productoImg = producto.querySelector('.card-img-top').src;
+//     // const newProducto = {
+//     //     titulo: productoTitulo,
+//     //     // precio: itemPrice,
+//     //     img: productoImg,
+//     //     cantidad: 1
+//     //     }
+//     addLocalStorage()
+//         Swal.fire({
+//         title: 'Tu pedido',
+//         text: 'Orden'+ btn.value,
+//         imageUrl: 'https://unsplash.it/400/200',
+//         imageWidth: 400,
+//         imageHeight: 200,
+//         imageAlt: 'Custom image',
+//     })
+//     carrito.push(Producto)
+// }
+// function addLocalStorage(){
+//   localStorage.setItem('carrito', JSON.stringify())
+//   console.log(carrito)
+// }
+
+// // fetch('https://jsonplaceholder.typicode.com/posts')
+// //   .then((response) => response.json())
+// //   .then((tabla) => mostrarTabla(tabla))
+// //   .catch(error => console.log(error))
+
+
+// // const mostrarTabla = (tabla)=>{
+// //     document.getElementById("tabla")
+    
+// //   }
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//   .then((response) => response.json()) 
+//   .then((tablas) => mostrarProducto(tablas));
+// const mostrarProducto = (tablas => {
+//     console.log(tablas)
+//     let body = ``
+//     for (let i = 0; i<5; i++){
+//         body += `<tr><td>${tablas[i].id}</td><td>${tablas[i].title}</td><td>${tablas[i].Precio}</td></tr>` 
+//     }
+//     document.getElementById("tablas").innerHTML = body
+// }) 
--- js/scrip.js
+++ js/scrip.js
@@ -1,57 +1,97 @@
-const Comprar = document.querySelectorAll(".combo")
-let carrito = []
+const contProductos = document.getElementById('contenedorProd')
+let carrito=[];
 
 
-Comprar.forEach(btn =>{
-    btn.addEventListener("click",agregarcarrito)
-})
-
-function agregarcarrito(e){
-    const button = e.target
-    const producto = button.closest(".card")
-    const productoTitulo = producto.querySelector('.card-title').textContent;
-    // const productoPrecio = producto.querySelector('.precio').textContent;
-    const productoImg = producto.querySelector('.card-img-top').src;
-    const newProducto = {
-        titulo: productoTitulo,
-        // precio: itemPrice,
-        img: productoImg,
-        cantidad: 1
-        }
-        carrito.push(newProducto)
-        console.log(addLocalStorage())
-        Swal.fire({
-        title: 'Tu pedido',
-        text: '-',
-        imageUrl: 'https://unsplash.it/400/200',
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
-    })
-}
-function addLocalStorage(){
-  localStorage.setItem('carrito', JSON.stringify(carrito))
-  console.log(carrito)
-}
-
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//   .then((response) => response.json())
-//   .then((tabla) => mostrarTabla(tabla))
-//   .catch(error => console.log(error))
-
-
-// const mostrarTabla = (tabla)=>{
-//     document.getElementById("tabla")
+const dataProductos = async()=>{
+    const resp = await fetch("./js/data.json");
+    const dataJson = await resp.json();
+    console.log(dataJson);
     
-//   }
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json()) 
-  .then((tablas) => mostrarProducto(tablas));
-const mostrarProducto = (tablas => {
-    console.log(tablas)
-    let body = ``
-    for (let i = 0; i<5; i++){
-        body += `<tr><td>${tablas[i].id}</td><td>${tablas[i].title}</td><td>${tablas[i].Precio}</td></tr>` 
-    }
-    document.getElementById("tablas").innerHTML = body
-}) 
+    let o = -1;
+    dataJson.forEach((element)=>{
+        contProductos.innerHTML +=`
+                <div class="card" style="width: 18rem;">
+                    <img src="${element.img}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text">$ ${element.precio}</p>
+                        <button type="button" class="btn btn-dark" id="agregar ${element.id}">Comprar</button>
+                    </div> 
+                </div>`
+    })
+
+    const btnProducto = document.querySelectorAll("button[type='button']")
+    btnProducto.forEach(btn => {
+        // Datajson forEach error, btnProducto addEvent... Error :(
+        btn.addEventListener("click", ()=>{
+            console.log(btn); 
+            
+            Swal.fire({
+                title: 'Tu pedido agregado al carrito  ' + " " + dataJson[0].title,
+                text: '$'+" "+ dataJson[0].precio,
+                imageUrl: dataJson[0].img,
+                imageWidth: 400,
+                imageHeight: 200,
+                imageAlt: 'Custom image',
+            })
+            const producto = {
+                title:dataJson[0].title,
+                precio:dataJson[0].precio,
+                img:dataJson[0].img,
+                id:dataJson[0].id,
+                cantidad:1
+            }
+            agregarProductoCarrito(producto)
+        });
+        function agregarProductoCarrito(producto){
+            for(let i = 0;i<carrito.length;i++){
+               if (carrito[i].title === producto.title){
+                carrito[i].cantidad ++;
+                let total = 0;
+                    const carritoTotal = document.querySelector(".total")
+                    carrito.forEach((producto)=>{
+                        let precio = producto.precio
+                        total = total + precio*producto.cantidad
+                    })
+                    carritoTotal.innerHTML =`$${total}`
+               }
+               
+            }
+           carrito.push(producto)
+           localStorage.setItem('Carrito', JSON.stringify(carrito))
+           renderCarrito();
+        }
+        const tbody = document.querySelector(".tbody")
+        function renderCarrito(){
+            tbody.innerHTML = ``
+            carrito.map(element =>{
+                const tr = document.createElement("tr")
+                tr.classList.add("itemCarrito")
+                const relleno = `
+                <td>${element.id}</td>
+                <td>${element.title}</td>
+                <td>${element.cantidad}</td>
+                <td>$ ${element.precio}</td>
+                <td><button type="button" class="btn btn-danger">Eliminar</button></td>
+
+                `
+                tr.innerHTML = relleno
+                tbody.append(tr)
+                tr.querySelector(".btn-danger")
+                tr.addEventListener("click",()=>{
+                    tr.remove()
+                    carrito.cantidad --;
+                    elimnarCarrito()
+                })
+                function elimnarCarrito() {
+                    carrito.splice(0)
+                    console.log(carrito);
+                }
+                
+            })
+            console.log(carrito);
+        }
+    });       
+}
+dataProductos()
+
